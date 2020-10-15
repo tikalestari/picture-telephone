2c66f21396224b2392c17e975aa0f5bdad9d2a37
tikalestari <tika.dwilestari@gmail.com>
2020-10-14 23:01:58 -0500

    create database structure, add getGames firebase function

--- pictureTelephone-functions/functions/index.js
+++ pictureTelephone-functions/functions/index.js
@@ -1,9 +1,24 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello world!");
+});
+
+exports.getGames = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('games').get()
+        .then(data => {
+            let games = [];
+            data.forEach(doc => {
+                games.push(doc.data());
+            });
+            return res.json(games);
+        })
+        .catch(err => console.error(err));
+});
